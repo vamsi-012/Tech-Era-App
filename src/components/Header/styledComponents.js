2eeaeaff7e068e0f3093bfbfbf09228f903e61a0
vamsi-012 <vamshikatikala1@gmail.com>
2023-02-07 14:50:13 +0000

    tech era app

---
+++ src/components/Header/styledComponents.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import styled from 'styled-components'
+
+export const NavHeader = styled.div`
+  background-color: #f1f5f9;
+`
+
+export const WebsiteLogo = styled.img`
+  height: 80px;
+  width: 250px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 60px;
+`
